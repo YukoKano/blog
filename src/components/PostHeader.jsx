2b7692d2f0d95f5c18d63050137bd2054e80efd5
YukoKano <halloween_1031@hotmail.co.jp>
2024-06-05 23:43:22 +0900

    date-fnsで日時を変換

--- src/components/PostHeader.jsx
+++ src/components/PostHeader.jsx
@@ -3,6 +3,7 @@ import styles from "@/styles/PostHeader.module.css";
 // ↓module not found になっていたが、yarnしたら治った パッケージ入れたらyarnする
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClock } from "@fortawesome/free-regular-svg-icons";
+import { ConvertDate } from "./ConvertDate";
 
 export const PostHeader = ({ title, subtitle, publish = "" }) => {
   return (
@@ -12,7 +13,7 @@ export const PostHeader = ({ title, subtitle, publish = "" }) => {
       {publish && (
         <div className={styles.publish}>
           <FontAwesomeIcon icon={faClock} size="lg" color="var(--gray-25)" />
-          {publish}
+          <ConvertDate dateISO={publish} />
         </div>
       )}
     </div>
