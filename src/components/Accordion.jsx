120e5eef2df14c1c0f164d324ea08997c9518689
YukoKano <halloween_1031@hotmail.co.jp>
2024-06-26 09:21:55 +0900

    Accordionのスタイル追加

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,18 +1,25 @@
+"use client";
+
 import styles from "@/styles/Accordion.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 export const Accordion = ({ heading, children }) => {
+  const [textIsOpen, setTextIsOpen] = useState(false);
+
+  const toggleText = () => {
+    setTextIsOpen((prev) => !prev);
+  };
   return (
-    <div className={styles.open}>
+    <div className={textIsOpen ? styles.open : styles.close}>
       <h3 className={styles.heading}>
         {/* h3にbutton……？ */}
-        <button>
+        <button onClick={toggleText}>
           {heading}
           <FontAwesomeIcon icon={faCircleChevronDown} className={styles.icon} />
         </button>
       </h3>
-      t
       <div className={styles.text}>
         <div className={styles.textInner}>{children}</div>
       </div>
